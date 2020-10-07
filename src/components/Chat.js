f9a43fe38119b6eda71225178c64195258865756
Kyle Hubbard <kyle.james.hubbard@gmail.com>
2020-10-07 09:28:20 -0700

    FINALLY FIGURE OUT HOW TO PRINT THINGS

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,36 +1,70 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useFirestore } from 'react-redux-firebase';
 // import Message from './Message';
 
 //shows information from chat with another participent and includes input and send button functionality
 
-function Chat(props) {
+function useMessages(dbTable) {
   const firestore = useFirestore();
+  const [messages, setMessages] = useState([])
+  useEffect(() => {
+    firestore.collection(`${dbTable}`)
+      .onSnapshot((snapshot) => {
+        const newMessages = snapshot.docs.map((doc) => ({
+          description: doc.data().description,
+          type: doc.data().type
+        }))
 
-  return (firestore.collection(`${props.chatName}`)
-    .get()
-    .then(function(query) {
-      let messageArray = []
-      query.docs.forEach(function(doc) {
-        const item = doc.data().description
-        messageArray.push(item);
-      });
-      console.log(messageArray)
-      return (
-        <React.Fragment>
-          <h1>This is the chat</h1>
-          {
-            messageArray.map((message, i) => {
-              // return <Message description={message.description} />
-            return <p key={i}>{message}</p>
-            })
-          }
-        </React.Fragment>
-      )
-    }).catch(function(error) {
-      console.log("catch tripped")
-      console.log(error);
-    }));
+        setMessages(newMessages);
+      })
+  }, [])
+
+  return messages
+}
+
+
+
+
+const Chat = (props) => {
+  // const firestore = useFirestore();
+  const conversation = useMessages(props.chatName);
+  console.log(conversation);
+  
+
+  return (
+    <React.Fragment>
+      {conversation.map((convo) => {
+        return <p>{convo.description}</p>
+      })}
+    </React.Fragment>
+  )
+
+
+
+  // return (firestore.collection(`${props.chatName}`)
+  //   .get()
+  //   .then(function(query) {
+  //     let messageArray = []
+  //     query.docs.forEach(function(doc) {
+  //       const item = doc.data().description
+  //       messageArray.push(item);
+  //     });
+  //     console.log(messageArray)
+  //     return (
+  //       <React.Fragment>
+  //         <h1>This is the chat</h1>
+  //         {
+  //           messageArray.map((message, i) => {
+  //             // return <Message description={message.description} />
+  //           return <p key={i}>{message}</p>
+  //           })
+  //         }
+  //       </React.Fragment>
+  //     )
+  //   }).catch(function(error) {
+  //     console.log("catch tripped")
+  //     console.log(error);
+  //   }));
 
   // return (
   //   <React.Fragment>
